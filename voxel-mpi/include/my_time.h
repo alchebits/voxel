707f4976545a5d298c97d2a75258d0c297d7b9f1
Darek <darek@aexol.com>
2015-04-28 16:56:19 +0200

    my time now on chrono lib

--- voxel-mpi/include/my_time.h
+++ voxel-mpi/include/my_time.h
@@ -1,7 +1,6 @@
 #ifndef MY_TIME_H_
 #define MY_TIME_H_
-#include <iostream>
-#include<sys/time.h>
+
 
 double nowSeconds();
 
--- voxel-mpi/src/my_time.cpp
+++ voxel-mpi/src/my_time.cpp
@@ -1,7 +1,13 @@
 #include "../include/my_time.h"
 
+#include <iostream>
+#include <chrono>
+
 double nowSeconds(){
-	static struct timeval tv;
-	gettimeofday(&tv, NULL);
-	return ((double)tv.tv_sec) + (tv.tv_usec/1000000.0);
+//	static struct timeval tv;
+//	gettimeofday(&tv, NULL);
+//	return ((double)tv.tv_sec) + (tv.tv_usec/1000000.0);
+    auto duration = std::chrono::system_clock::now().time_since_epoch();
+    auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+    return millis / 1000.0;
 }
